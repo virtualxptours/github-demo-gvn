cb4abfb1ef7c29c247dbf4cba6173a1b1de9071e
virtualxptours <127051187+virtualxptours@users.noreply.github.com>
2024-01-17 11:34:57 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+/*!
+* Start Bootstrap - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
+*/
+window.addEventListener('DOMContentLoaded', () => {
+    let scrollPos = 0;
+    const mainNav = document.getElementById('mainNav');
+    const headerHeight = mainNav.clientHeight;
+    window.addEventListener('scroll', function() {
+        const currentTop = document.body.getBoundingClientRect().top * -1;
+        if ( currentTop < scrollPos) {
+            // Scrolling Up
+            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-visible');
+            } else {
+                console.log(123);
+                mainNav.classList.remove('is-visible', 'is-fixed');
+            }
+        } else {
+            // Scrolling Down
+            mainNav.classList.remove(['is-visible']);
+            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-fixed');
+            }
+        }
+        scrollPos = currentTop;
+    });
+});
+var img1 = document.getElementById("img1"),
+    img2 = document.getElementById("img2");
+
+img1.onmouseover = function(){
+    img2.style.display = "block";
+}
+
+img1.onmouseout = function(){
+    img2.style.display = "none";
+}
